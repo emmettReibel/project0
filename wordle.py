b974ecbba04368b6e34309d22ea61fbdb3108fa3
emmett reibel <emmett.reibel@gmail.com>
2024-08-21 16:37:01 +0000

    wordle game complete!

--- wordle.py
+++ wordle.py
@@ -1,21 +1,23 @@
 # TASK A: Define a variable 'word' that holds the correct word for the wordle game
-word = "crown"
+word = "CROWN"
 
 # TASK B: Define a function 'makeAGuess()' that passes in a users guess as a parameter
 def makeAGuess(guess):
   
 
   # TASK C:Define a variable 'hint' that holds an empty string
- hint = ""
+  hint = ""
 
   # TASK D: Build a loop that loops from 0 to the length of word
   for i in range(len(word)):
     if guess[i] == word[i]:
-      hint += "g"
+      hint += "G"
+    elif guess[i] in word:
+      hint += "Y"
     else:
-      continue
+      hint += "-"
+  return hint
     # TASK E: Check if the current letter of guess matches the current letter of word. If so add the letter "G" to the hint
-    
     
     # TASK F: If the previous condition is fales, check if the current letter of guess is in word at all. If so add the letter "Y" to the hint
     
@@ -23,13 +25,14 @@ def makeAGuess(guess):
     # TASK G: If the previous two conditions are false, add the symbol "-" to the hint
     
   # TASK H: Return hint
-  
-
-
-print("Let's play wordle! /n Guess the Wordle in 6 tries. Each guess must be a valid 5-letter word. For each guess, a hint will tell you how many letters you've guessed correctly. A G represents a letter in the word and in the correct spot.. A Y represents a letter in the word but in the wrong spot. A - represents a letter not in the word in any spot. \n Guess below! \n")
-
+guesslimit = 0
 # TASK I: Build a loop that loops 6 times (representing the number of guesses a user has)
-
+while guesslimit < 6:
+  ehint = makeAGuess(input(f"make a 5 letter guess! you have {str(6-guesslimit)} guesses left. ").upper())
+  print(ehint)
+  guesslimit += 1
+  if guesslimit == 6 and ehint != "GGGGG":
+    print("You lose! Try again.")
 
   # TASK J: Define a variable 'guess'. prompt the user for their 5-letter guess and store it in the variable
 
