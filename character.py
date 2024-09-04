0d03815e68d4ade3689b039c84ef5cf58f6cbc80
emmett reibel <emmett.reibel@gmail.com>
2024-09-04 16:55:47 +0000

    added more lines, having issues with methods that idk how to deal with

--- character.py
+++ character.py
@@ -10,13 +10,13 @@ class Character:
     def talk_to_player(self, player_name, player_msg):
         if self.name == "Samuel":
             if self.received == False:
-                print(f"Hey {player_name}, my name is {self.name}. We need your help to throw this tea overboard! Will you help us?")
+                print(f"{self.name} says: Apologies, {player_name}! My name is {self.name}. We need your help to throw this tea overboard!")
                 YN = input("Will you help the revolutionary? Y/N: ").upper()
 
                 if YN == "Y":
-                    print(f"{self.name} says: Thank you, {player_name}! Take this crate of tea and throw it overboard!")
+                    print(f"{self.name} says: Thank you, {player_name}! Take this crate of tea and toss it to the waves!")
                     self.received = True
-                    self.inventory.append("tea_crate")
+                    player.inventory.append("tea_crate")
 
                 else:
                     print(f"{self.name} says: You are a coward, {player_name}!")
--- runner.py
+++ runner.py
@@ -1,18 +1,21 @@
-# from player import Player
-# from item import Item  
-# from character import Character
+from player import Player
+from item import Item  
+from character import Character
 from wordle import makeAGuess
 from wordle import playGame
 
-# print("What an interesting looking ship in the distance, with well dressed individuals surrounding it menacingly...")
-# player_name = input("What is my name? ")
-# player = Player(player_name, 100)
+print("What an interesting looking ship in the distance, with well dressed individuals surrounding it menacingly...")
+player_name = input("What is my name? ")
+player = Player(player_name, 100)
 
 
-# decision1 = input ("You ponder whether or not you should approach the ship. Do you want to approach the ship? Y/N: ").upper() 
-# if decision1 == "Y":
-    # print("You approach the ship and are greeted by a tall, aristocratic man.")
-    # character = Character("Samuel", 100)
-# else:
-    # print("You look away from the ship, and continue on your way to Boston.")
-playGame()
+decision1 = input ("You ponder whether or not you should approach the ship. Do you want to approach the ship? Y/N: ").upper() 
+if decision1 == "Y":
+    print("You approach the ship and are greeted by a tall, aristocratic man.")
+    character = Character("Samuel", 100)
+    decision2 = input("The man says: Hello, odd individual who is suspiciously close to my covert operation! What's your name? (tell him? Y/N:) ")
+    if decision2 == "Y":
+        character.talk_to_player(self, player, "x")
+    
+else:
+    print("You look away from the ship, and continue on your way to Boston.")
